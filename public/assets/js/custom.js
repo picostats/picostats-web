2ff2f7feb36d8435fd796313c17fb3d98615941b
Tihomir Piha <piha.tihomir@gmail.com>
2017-03-19 05:45:22 +0100

    marking active link

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,5 +1,5 @@
 for (var i = 0; i < document.links.length; i++) {
-    if (document.links[i].href == document.URL && !document.links[i].className.includes('skip-active')) {
+    if ((document.URL == document.links[i].href || (document.URL.includes(document.links[i].href) && document.links[i].href != window.location.origin + "/")) && !document.links[i].className.includes('skip-active')) {
         document.links[i].parentNode.className = 'active';
         if (document.links[i].parentNode.parentNode.className.includes('treeview-menu')) {
             document.links[i].parentNode.parentNode.parentNode.className = 'active';
